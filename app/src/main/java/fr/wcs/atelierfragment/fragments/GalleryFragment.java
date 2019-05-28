abc51a068fec8cc8a087cb3fdef9aa659689bdbf
bastienwcs <email>
2019-05-28 13:52:19 +0200

    init

---
+++ app/src/main/java/fr/wcs/atelierfragment/fragments/GalleryFragment.java
@@ -0,0 +1,50 @@
+package fr.wcs.atelierfragment.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import fr.wcs.atelierfragment.R;
+
+public class GalleryFragment extends Fragment {
+
+    GalleryListener listener;
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        listener = (GalleryListener) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+
+        Button btHome = view.findViewById(R.id.btHome);
+        btHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onHomeClicked();
+            }
+        });
+
+        return view;
+    }
+
+    public interface GalleryListener {
+
+        void onHomeClicked();
+
+    }
+}
